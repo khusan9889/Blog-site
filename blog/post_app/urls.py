4661e64220f5cdecf75bcbc749c94d40a5adf198
khusan9889 <k.xusan_2003@mail.ru>
2022-06-08 18:00:02 +0500

    authentication checkiing for users who want to make changes in posts

--- blog/post_app/urls.py
+++ blog/post_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AddCategoryView, AddPostView, DeletePostView, HomeView, ArticleDetailView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
+from .views import AddCategoryView, AddPostView, CategoryListView, DeletePostView, HomeView, ArticleDetailView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView
 
 
 urlpatterns = [
@@ -10,5 +10,6 @@ urlpatterns = [
   path('article/<int:pk>/remove' , DeletePostView.as_view(), name = 'delete_post'),
   path('add_category/', AddCategoryView.as_view(), name= 'add_category'),
   path('category/<str:cats>/', CategoryView, name='category'),
+  path('category-list/', CategoryListView, name = 'category-list'),
 
 ]
--- blog/post_app/views.py
+++ blog/post_app/views.py
@@ -14,10 +14,23 @@ class HomeView(ListView):
     template_name = 'home.html'
     ordering = ['-created_on']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
 
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
 
 class AddPostView(CreateView):
     model = Post
@@ -45,4 +58,9 @@ class AddCategoryView(CreateView):
 def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats.replace('-', ' '))
     return render(request, 'categories.html', {'cats':cats.title().replace('-', ' '), 'category_posts': category_posts})
+
+
+def CategoryListView(request):
+    cat_menu_list = Category.objects.all()
+    return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
 
