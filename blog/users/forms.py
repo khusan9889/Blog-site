3c92c5ea5c27a8567a0cc967f7b4e3e3bdcd7571
khusan9889 <k.xusan_2003@mail.ru>
2022-06-09 14:51:28 +0500

    modified registration form

--- blog/users/forms.py
+++ blog/users/forms.py
@@ -1,2 +1,20 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
+from django import forms
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Title of your post'}))
+    first_name = forms.CharField(max_length=100, widget= forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title of your post'}))
+    last_name = forms.CharField(max_length=100, widget= forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title of your post'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__( *args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
