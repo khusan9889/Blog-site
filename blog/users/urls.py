5b84237da2bd7af0c0d8b4ce9e0e36ac1a646cb8
khusan9889 <k.xusan_2003@mail.ru>
2022-06-13 10:33:53 +0500

    style Profile Page with bootstrap

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserEditView
 
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name = 'register'),
+    path('edit_profile/', UserEditView.as_view(), name = 'edit_profile'),
     
-
 ]
--- blog/users/views.py
+++ blog/users/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+
+    def get_object(self):
+        return self.request.user
