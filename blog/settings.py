7fdc7337d244dba5963c944f31ea42ae10f2f65c
khusan9889 <k.xusan_2003@mail.ru>
2022-07-13 18:04:36 +0500

    static root added

--- blog/settings.py
+++ blog/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS  = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
