5a03dd491cf281196e70bfa2dee93e744f532c79
khusan9889 <k.xusan_2003@mail.ru>
2022-07-13 18:05:52 +0500

    static finders added

--- blog/settings.py
+++ blog/settings.py
@@ -139,6 +139,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'media'),
 )
 
+STATICFILES_FINDERS=["django.contrib.staticfiles.finders.FileSystemFinder", "django.contrib.staticfiles.finders.AppDirectoriesFinder"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
